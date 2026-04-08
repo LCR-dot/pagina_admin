674f2380b91353c92a85f742de22fa2c15c31dcb
Laura Colín <lcolin01@gmail.com>
2026-04-07 18:29:06 -0600

    Proyecto pagina admin actualizada

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('MYSQLDATABASE', default='railway'),
         'USER': config('MYSQLUSER', default='root'),
-        'PASSWORD': config('MYSQLPASSWORD', default='FDaKPqHLRShAKjPhkIhgvTKgPerVTAvk'),
+        'PASSWORD': config('MYSQLPASSWORD', default='oblFdtGqxzkrKXMcFOIMnwlhEpVwGTHs'),
         'HOST': config('MYSQLHOST', default='mysql.railway.internal'),
         'PORT': config('MYSQLPORT', default='3306'),
         'OPTIONS': {
